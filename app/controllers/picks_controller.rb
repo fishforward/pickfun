01d4970fb6d40ce8d46f6d6187ef98897a003bc2
fishforward <yy_8789@126.com>
2011-12-19 01:31:40 +0800

    commit-20111218-2

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -13,23 +13,42 @@ class PicksController < ApplicationController
   # GET /picks/1
   # GET /picks/1.xml
   def show
-    @pick = Pick.find(params[:id])
+    pickId = params[:id]
+    id = pickId.split('_')
+    @l_pic = Pic.find(id[0])
+    @r_pic = Pic.find(id[1])
+
+    #进度条获取
+    @pickPercentage = Pic.count(:all, :conditions =>["wins > 0 or losses > 0"] );
+    @picSum = Pic.count(:all);
+    
+    # 菜单标签配置
+    @Home_current = true;
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @pick }
+      format.html # new.html.erb
+      format.xml  { render :xml => @l_pic | @r_pic | @pickPercentage | @picSum | @Home_current}
     end
   end
 
   # GET /picks/new
   # GET /picks/new.xml
   def new
-    @pick = Pick.new
+    @l_pics = Pic.find(:all, :order => "RANDOM() LIMIT 1");
+    l_id = @l_pics[0].id;
+    @r_pics = Pic.find(:all, :conditions =>["id <> ?", l_id], :order => "RANDOM() LIMIT 1");
+    
+    @l_pic = @l_pics[0];
+    @r_pic = @r_pics[0];
+    
+    # 菜单标签配置
+    @Home_current = true;
+    pickId = @l_pic.id.to_s + "_" + @r_pic.id.to_s
 
     respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @pick }
+      format.html { redirect_to :action => "show", :id => pickId}
     end
+    
   end
 
   # GET /picks/1/edit
@@ -40,18 +59,33 @@ class PicksController < ApplicationController
   # POST /picks
   # POST /picks.xml
   def create
-    @pick = Pick.new(params[:pick])
-
-    respond_to do |format|
-      if @pick.save
-        flash[:notice] = 'Pick was successfully created.'
-        format.html { redirect_to(@pick) }
-        format.xml  { render :xml => @pick, :status => :created, :location => @pick }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @pick.errors, :status => :unprocessable_entity }
-      end
-    end
+    @win_pic_id = params[:l_pic_id];
+    @lose_pic_id = params[:r_pic_id];
+    
+    @pick = Pick.new
+    @pick.user = 'mySelf';
+    @pick.ip = request.remote_ip;
+    
+    @pick.win_pic_id = @win_pic_id;
+    @pick.loss_pic_id = @lose_pic_id;
+         
+     pp = Pic.new;
+     pp.pk(@win_pic_id, @lose_pic_id);
+     #winPic.pk(@lose_pic_id);
+     
+     puts @win_pic_id
+      
+     respond_to do |format|
+       if @pick.save
+          
+            flash[:notice] = 'Have Fun Pick Pic!  winner: ' + @win_pic_id + ",losser:" + @lose_pic_id ;
+            format.html { redirect_to(:action =>'new')};
+           # format.xml  { render :xml => @picks }
+        else
+            format.html { redirect_to(:action =>'new')};
+            format.xml  { render :xml => @pic.errors, :status => :unprocessable_entity }
+        end
+     end
   end
 
   # PUT /picks/1
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -30,5 +30,68 @@ class Pic < ActiveRecord::Base
     paginate :per_page => 10, :page => page,  
              :order => 'scores desc'  
    end 
+   
+   
+     def pk(winPicId, losePicId)
+    winPic = Pic.find(winPicId);
+    losePic = Pic.find(losePicId);
+    
+    record(winPic, losePic);
+#    puts winPic;
+#    puts losePic;
+  end
+
+  def record(winPic, losePic)
+    #winPic.scores = winPic.scores + 1;
+    winExpectation = getExpectation(winPic.scores, losePic.scores, 1);
+    #puts 'winExpectation:'+winExpectation.to_s;
+    winPic_scores = getNewScores(winPic.scores, winExpectation, 1);
+    #puts 'win_s:' + winPic_scores.to_s;
+   
+ 
+    #losePic.scores = losePic.scores - 1;
+    loseExpectation = getExpectation(winPic.scores, losePic.scores, -1);
+    #puts 'loseExpectation:' + loseExpectation.to_s;
+    losePic_scores = getNewScores(losePic.scores, loseExpectation, 0);
+    #puts 'lose_s:' + losePic_scores.to_s;
+    
+    winPic.scores = winPic_scores
+    winPic.wins = winPic.wins + 1;
+    if !winPic.save
+      #puts 'winPic.save exception'
+      #throw Exception("update ex");
+    end
+    
+    losePic.scores = losePic_scores;
+    losePic.losses = losePic.losses + 1; 
+    if !losePic.save
+      #puts 'losePic.save exception'
+      #throw Exception("update ex");
+    end
+  end
+  
+
+  ## 计分 - 获取期望值 
+  def getExpectation(winScores,loseScores,flag)
+   return 1 / (1 + 10 ** ((loseScores - winScores) * flag / 400.to_f));
+  end
+  
+  ## 计分 - 获取本次PK分值
+  def getNewScores(oldScores,expectation,sa )
+    return oldScores + 16.to_f * ( sa - expectation);
+  end
+  
+  #获取picked的pic数据
+  def self.getPickProgress
+    
+    #pick百分比
+     Pic.count(:all, :conditions =>["wins > 0 or losses > 0"] );
+    
+  end
+  
+  #获取pic总数
+  def self.allPicSum
+    Pic.count(:all);
+  end
               
 end
